b813bf6f159fdce64e2a01339cc743184a9bb396
LastHope777 <nikita_pishkov@mail.ru>
2024-07-11 12:25:07 +0400

    Добавьте файлы проекта.

---
+++ InterestingMenu/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Choose your destiny:");
+
+int top = Console.CursorTop;
+int y = top;
+
+Console.WriteLine("One");
+Console.WriteLine("Two");
+Console.WriteLine("Three");
+
+int down = Console.CursorTop;
+
+Console.CursorSize = 100;
+Console.CursorTop = top;
+
+ConsoleKey key;
+while ((key = Console.ReadKey(true).Key) != ConsoleKey.Enter)
+{
+    if (key == ConsoleKey.UpArrow)
+    {
+        if (y > top)
+        {
+            y--;
+            Console.CursorTop = y;
+        }
+    }
+    else if (key == ConsoleKey.DownArrow)
+    {
+        if (y < down - 1)
+        {
+            y++;
+            Console.CursorTop = y;
+        }
+    }
+}
+
+Console.CursorTop = down;
+
+if (y == top)
+    Console.WriteLine("один");
+else if (y == top + 1)
+    Console.WriteLine("два");
+else if (y == top + 2)
+    Console.WriteLine("три");
